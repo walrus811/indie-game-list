e25753b01fac17a3055feb0261e64ffb811f75cb
walrus811 <walrusray811@gmail.com>
2021-11-24 17:57:05 +0900

    데이터 추가

---
+++ js/data.js
@@ -0,0 +1,251 @@
+export const WEBZIN_DATA = [
+  {
+    name: "The Binding of Isaac : Rebirth",
+    genre: "액션,RPG,탑뷰 슈팅",
+    playTime: 5,
+    localization : true
+  },
+  {
+    name: "Furi",
+    genre: "액션,보스 러쉬",
+    playTime: 5.5,
+    localization : true
+  },
+  {
+    name: "Braid",
+    genre: "플랫폼,퍼즐",
+    playTime: 5,
+    localization : true
+  },
+  {
+    name: "Superhot",
+    genre: "액션,퍼즐,공포",
+    playTime: 2.5,
+    localization : true
+  },
+  {
+    name: "Rise & Shine",
+    genre: "액션,어드벤처",
+    playTime: 2.5,
+    localization : true
+  },
+  {
+    name: "Hotline Miami",
+    genre: "탑다운,액션,슈터",
+    playTime: 5,
+    localization : true
+  },
+  {
+    name: "To the Moon",
+    genre: "어드벤처,RPG",
+    playTime: 4,
+    localization : true
+  },
+  {
+    name: "Firewatch",
+    genre: "어드벤처,1인칭",
+    playTime: 4,
+    localization : true
+  },
+  {
+    name: "Nidhogg",
+    genre: "대전",
+    playTime: 1,
+    localization : false
+  },
+  {
+    name: "The Stanley Parable",
+    genre: "1인칭,어드벤처",
+    playTime: 1.5,
+    localization : true
+  },
+  {
+    name: "Outlast",
+    genre: "1인칭,호러",
+    playTime: 5,
+    localization : true
+  },
+  {
+    name: "Papers,Please",
+    genre: "텍스트,퍼즐,시뮬레이션",
+    playTime: 5,
+    localization : true
+  },
+  {
+    name: "Limbo",
+    genre: "플랫폼,퍼즐,스크롤",
+    playTime: 3,
+    localization : true
+  },
+  {
+    name: "Rocket League",
+    genre: "레이싱,스포츠",
+    playTime: 4,
+    localization : true
+  },
+  {
+    name: "Into the Breach",
+    genre: "턴제,RPG",
+    playTime: 5.5,
+    localization : true
+  },
+  {
+    name: "Devil Daggers",
+    genre: "1인칭,액션,슈팅,호러",
+    playTime: 5.5,
+    localization : false
+  },
+  {
+    name: "Pony Island",
+    genre: "퍼즐",
+    playTime: 2.5,
+    localization : true
+  },
+  {
+    name: "Doki Doki Literature Club",
+    genre: "텍스트,비주얼 노벨",
+    playTime: 4,
+    localization : true
+  },
+  {
+    name: "What Remains of Edith Finch",
+    genre: "어드벤처",
+    playTime: 2,
+    localization : true
+  }
+];
+
+export const EDITORS_PICKS = [
+  {
+    name: "Deemo -Reborn-",
+    genre: "리듬,어드벤처",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "쥐돌이 버스터즈",
+    genre: "어드벤처",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "쥐돌이 버스터즈",
+    genre: "어드벤처",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "오리DUCK후",
+    genre: "캐주얼,방치",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "츠키의 모험",
+    genre: "어드벤처,방치",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "크랩 전쟁",
+    genre: "어드벤처,스크롤,슈팅",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "Beholder",
+    genre: "어드벤처",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "Like A Dino!",
+    genre: "리듬",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "고양이는 정말 팡팡",
+    genre: "퍼즐",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "팬텀 로즈 스칼렛",
+    genre: "로그라이크,카드,어드벤처",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "룸즈! : 장난감 장인의 저택",
+    genre: "퍼즐,플랫폼",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "Dadish 2",
+    genre: "플랫폼,액션",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "Box It Up! Inc.",
+    genre: "퍼즐",
+    count_apps: 5,
+    count_play: 5,
+    description : "구글 플레이 인디게임 페스티벌 수상작(TOP 10)"
+  },
+  {
+    name: "pink (game)",
+    genre: "퍼즐",
+    count_apps: 5,
+    count_play: 5,
+    description : ""
+  },
+  {
+    name: "고양이와 스프",
+    genre: "방치형,시뮬레이션",
+    count_apps: 5,
+    count_play: 5,
+    description : "구글 플레이 인디게임 페스티벌 수상작(TOP 3)"
+  }
+];
+
+export const IN_TWO_HUNDREDS = [
+  {
+    name: "창술사 키우기",
+    rank : [null,null,null,null,75],
+  },
+  {
+    name: "사신키우기 온라인",
+    rank : [56,72,71,71,76],
+  },
+  {
+    name: "슬레이어 키우기",
+    rank : [130,200,34,57,92],
+  },
+  {
+    name: "마피아42",
+    rank : [100,108,115,73,112],
+  },
+  {
+    name: "창술사 키우기",
+    rank : [null,null,null,114,144],
+  },
+  {
+    name: "좀비 고등학교",
+    rank : [173,151,133,148,181],
+  },
+];
